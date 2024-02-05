976f1e5d89261babd9db2c6959876702afb57dbb
bigfanoftim <bigfanoftim@gmail.com>
2024-02-05 10:31:46 +0900

    초기 세팅

---
+++ src/main/java/me/bigfanoftim/cachingcontext/SpringApplicationContextCachingTestApplication.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.cachingcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationContextCachingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationContextCachingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bigfanoftim/cachingcontext/SpringApplicationContextCachingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bigfanoftim.cachingcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class SpringApplicationContextCachingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
